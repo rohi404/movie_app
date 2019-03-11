a1806ed098f674225b92a2415f53877f3061dcd6
rohi <rohi040404@gmail.com>
2019-03-11 13:35:21 +0900

    Updates

---
+++ precache-manifest.66eba00ff1b0336ac1cabdf74753d59e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.7952d9d4.js"
+  },
+  {
+    "revision": "82cce102ddcb89a1d2da",
+    "url": "/movie_app/static/js/main.82cce102.chunk.js"
+  },
+  {
+    "revision": "13597e56bd0d4c2f4637",
+    "url": "/movie_app/static/js/2.13597e56.chunk.js"
+  },
+  {
+    "revision": "82cce102ddcb89a1d2da",
+    "url": "/movie_app/static/css/main.1977230b.chunk.css"
+  },
+  {
+    "revision": "d968dd021c01421815b178a5f5aad550",
+    "url": "/movie_app/index.html"
+  }
+];
